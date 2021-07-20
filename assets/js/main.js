5b488895ef0c04d7f891d09a93f17e42d7de938a
Tim Sch <timsch3@gmail.com>
2021-07-20 14:00:26 +0200

    init

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+function compareAges() {
+    document.getElementById("result").innerHTML = "Your age difference is " + Math.abs(document.getElementById("age1").value - document.getElementById("age2").value) + " years."
+}
